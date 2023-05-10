0e064eafd9c982b08378d000ae8f3a94cd0502a8
helpingstar <54899900+helpingstar@users.noreply.github.com>
2023-05-10 16:12:07 +0900

    update block, info rendering

--- gym_game2048/envs/game2048.py
+++ gym_game2048/envs/game2048.py
@@ -5,26 +5,17 @@ import gymnasium as gym
 from gymnasium import spaces
 
 class Game2048(gym.Env):
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 15}
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 10}
 
     def __init__(self, render_mode=None, size=4, goal=2048):
         self.size = size
         self.window_width = 400
         self.window_height = 500
+        self.goal = goal
+        self.best_score = 0
 
-        self.temp_window_margin = 10
-
-        self.board_margin = int((self.window_width - 2 * self.temp_window_margin) / (8 * size + 1))
-        self.block_size = self.board_margin * 7
-
-        self.board_size = self.board_margin * (size+1) + self.block_size * size
-        self.window_margin = (self.window_width - self.board_size) // 2
-
-        self.block_rect = (self.block_size, self.block_size)
-
-        self.left_top_board = np.array([self.window_margin, self.window_height-self.window_margin-self.board_size])
-        self.left_top_first_block = self.left_top_board + np.array([self.board_margin, self.board_margin])
-        self.to_next_block = self.block_size + self.board_margin
+        # number of episode
+        self.n_episode = 0
 
         assert math.log2(goal).is_integer() and goal > 4, "Goal must be a power of 2 and bigger than 4."
 
@@ -34,16 +25,6 @@ class Game2048(gym.Env):
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
-
-        # rednering setting
-        block_color = ["#eee4da", "#ede0c8", "#f2b179", "#f59563", "#f67c5f",
-                       "#f65e3b", "#edcf72", "#edcc61", "#edc850", "#edc53f", "#edc22e"]
-        self.game_color = {2 ** i: pygame.Color(color) for i, color in enumerate(block_color, 1)}
-        self.game_color[0] = pygame.Color("#cdc1b4")
-        self.game_color['background'] = pygame.Color("#faf8ef")
-        self.game_color['board_background'] = pygame.Color("#bbada0")
-        self.game_color['number_0'] = pygame.Color('#776e65')
-
 
         self.window = None
         self.clock = None
@@ -60,6 +41,10 @@ class Game2048(gym.Env):
         self.score = 0
         # score per step
         self.score_per_step = 0
+        # check_step
+        self.n_step = 0
+
+        self.n_episode += 1
 
         observation = self._get_obs()
         info = self._get_info()
@@ -93,8 +78,12 @@ class Game2048(gym.Env):
         # 2: up
         # 3: down
 
+        # increase n_step
+        self.n_step += 1
+
         self.score_per_step = 0
 
+        # Checks if the action is valid.
         is_changed = False
 
         if action < 2:
@@ -114,21 +103,24 @@ class Game2048(gym.Env):
 
         self.score += self.score_per_step
 
+        # If the goal is reached, set the reward to 1.
+        if self._is_reach_goal():
+            self._update_best_score()
+            return self._get_obs(), 1, True, False, self._get_info()
+
         if is_changed:
             self.spawnblock()
             terminated = self._is_game_over()
-            reward = terminated ^ 1
         else:
-            reward = 0
             terminated = False
-
-        observation = self._get_obs()
-        info = self._get_info()
 
         if self.render_mode == "human":
             self._render_frame()
 
-        return observation, reward, terminated, False, info
+        if terminated:
+            self._update_best_score()
+        # If the goal is not reached, set the reward to 1.
+        return self._get_obs(), 0, terminated, False, self._get_info()
 
     def _combiner(self, line:np.ndarray, way:int) -> np.ndarray:
         """Combine identical blocks on a single line.
@@ -197,9 +189,52 @@ class Game2048(gym.Env):
                     return False
         return True
 
+    def _update_best_score(self):
+        self.best_score = max(self.best_score, self.score)
+
+    def _is_reach_goal(self):
+        return np.any(self.board == self.goal)
+
     def render(self):
         if self.render_mode == "rgb_array":
             return self._render_frame()
+
+    def _render_block(self, r, c, canvas: pygame.Surface):
+        n_log = int(math.log2(self.board[r][c])) if self.board[r][c] != 0 else 0
+        left_top_this_block = (self.left_top_first_block + np.array([self.to_next_block * c, self.to_next_block * r]))
+        pygame.draw.rect(
+            canvas,
+            self.block_color[n_log],
+            (left_top_this_block, (self.block_rect))
+        )
+        # Empty parts do not output a number.
+        if self.board[r][c] == 0:
+            return
+
+        if n_log < 7:
+            size = self.block_font_size[0]
+        elif n_log < 10:
+            size = self.block_font_size[1]
+        else:
+            size = self.block_font_size[2]
+        font = pygame.font.Font(None, size)
+
+        # render text
+        color = self.block_font_color[0] if n_log < 3 else self.block_font_color[1]
+        text = font.render(str(self.board[r][c]), True, color)
+        text_rect = text.get_rect(center=(left_top_this_block + np.array([self.block_size//2, self.block_size//2])))
+        canvas.blit(text, text_rect)
+
+    def _render_info(self, canvas):
+        score = self.info_font.render(f'score: {self.score}', True, (119, 110, 101))
+        best_score = self.info_font.render(f'best: {self.best_score}', True, (119, 110, 101))
+        n_episode = self.info_font.render(f'episode: {self.n_episode}', True, (119, 110, 101))
+        n_step = self.info_font.render(f'step: {self.n_step}', True, (119, 110, 101))
+
+        canvas.blit(score, (20, 25))
+        canvas.blit(best_score, (20, 65))
+        canvas.blit(n_episode, (self.window_width // 2 - 15, 25))
+        canvas.blit(n_step, (self.window_width // 2 - 15, 65))
 
     def _render_frame(self):
         if self.window is None and self.render_mode == "human":
@@ -209,7 +244,37 @@ class Game2048(gym.Env):
             self.window = pygame.display.set_mode(
                 (self.window_width, self.window_height)
             )
-            self.font = pygame.font.Font(None, 50)
+
+            # rendering : Size
+            self.temp_window_margin = 10
+
+            self.board_margin = int((self.window_width - 2 * self.temp_window_margin) / (8 * self.size + 1))
+            self.block_size = int((self.window_width - 2 * self.temp_window_margin) / (8 * self.size + 1) * 7)
+
+            self.board_size = self.board_margin * (self.size+1) + self.block_size * self.size
+            self.window_margin = (self.window_width - self.board_size) // 2
+
+            self.block_rect = (self.block_size, self.block_size)
+
+            self.left_top_board = np.array([self.window_margin, self.window_height-self.window_margin-self.board_size])
+            self.left_top_first_block = self.left_top_board + np.array([self.board_margin, self.board_margin])
+            self.to_next_block = self.block_size + self.board_margin
+
+            # rendering: Block Color
+            self.block_color = [(205, 193, 180), (238, 228, 218), (237, 224, 200), (242, 177, 121),
+                                (245, 149, 99),  (246, 124, 95),  (246, 94, 59),   (237, 207, 114),
+                                (237, 204, 97),  (237, 200, 80),  (237, 197, 63),  (237, 194, 46)]
+            self.game_color = {}
+            self.game_color['background'] = pygame.Color("#faf8ef")
+            self.game_color['board_background'] = pygame.Color("#bbada0")
+            self.block_font_color = [(119, 110, 101), (249, 246, 242)]
+
+            # rendering: Block Font Size
+            self.block_font_size = [int(self.block_size * rate) for rate in [0.7, 0.6, 0.45]]
+
+            # rendering: Info
+            self.info_font = pygame.font.Font(None, 30)
+
         if self.clock is None and self.render_mode == "human":
             self.clock = pygame.time.Clock()
 
@@ -225,22 +290,9 @@ class Game2048(gym.Env):
         for r in range(self.size):
             for c in range(self.size):
                 # render block
-                left_top_this_block = (self.left_top_first_block + np.array([self.to_next_block * c, self.to_next_block * r]))
-                pygame.draw.rect(
-                    canvas,
-                    self.game_color[self.board[r][c]],
-                    (left_top_this_block, (self.block_rect))
-                )
-                # Empty parts do not output a number.
-                if self.board[r][c] == 0:
-                    continue
-                # render text
-                text = self.font.render(str(self.board[r][c]), True, self.game_color['number_0'])
-                text_rect = text.get_rect(center=(left_top_this_block + np.array([self.block_size//2, self.block_size//2])))
-                canvas.blit(text, text_rect)
+                self._render_block(r, c, canvas)
 
-        score = self.font.render(f'score: {self.score}', True, self.game_color['number_0'])
-        canvas.blit(score, (self.window_width // 3 * 1, self.left_top_board[1] // 2))
+        self._render_info(canvas)
 
         if self.render_mode == "human":
             # The following line copies our drawings from `canvas` to the visible window
