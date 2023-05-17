62778b0aeb26977667eb94a0077c3f576562915b
helpingstar <iamhelpingstar@gmail.com>
2023-05-18 01:55:21 +0900

    update reward_converter wrapper

--- gym_game2048/wrappers/reward_converter.py
+++ gym_game2048/wrappers/reward_converter.py
@@ -3,13 +3,16 @@ import numpy as np
 
 
 class RewardConverter(gym.RewardWrapper):
-    def __init__(self, env, terminated_reward=5.0, other=-0.001):
+    def __init__(self, env, goal=5.0, fail=-5, other=-0.001):
         super().__init__(env)
+        self.goal = goal
+        self.fail = fail
         self.other = other
-        self.terminated_reward = terminated_reward
 
     def reward(self, reward):
         if reward == 0:
             return self.other
+        elif reward > 0:
+            return self.goal
         else:
-            return reward * self.terminated_reward
+            return self.fail
