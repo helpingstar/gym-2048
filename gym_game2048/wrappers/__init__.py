c55ef8f32514a4182cd83c2dcb42f337d32effda
helpingstar <54899900+helpingstar@users.noreply.github.com>
2023-05-17 22:14:55 +0900

    fix wrapper __init__

--- gym_game2048/wrappers/__init__.py
+++ gym_game2048/wrappers/__init__.py
@@ -1,2 +1,3 @@
-from gym_game2048.wrappers.obs_like_game import ObsLikeGame
 from gym_game2048.wrappers.normalize_2048 import Normalize2048
+from gym_game2048.wrappers.preprocess_for_tensor import PreprocessForTensor
+from gym_game2048.wrappers.reward_converter import RewardConverter
