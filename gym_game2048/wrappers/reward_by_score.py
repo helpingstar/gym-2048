ee0068813ddde04c2919466ddbb270c32169d5e0
helpingstar <iamhelpingstar@gmail.com>
2023-05-21 22:01:57 +0900

    add goal_bonus in reward_by_score wrappers

--- gym_game2048/wrappers/reward_by_score.py
+++ gym_game2048/wrappers/reward_by_score.py
@@ -3,9 +3,10 @@ import numpy as np
 
 
 class RewardByScore(gym.Wrapper):
-    def __init__(self, env, log=True):
+    def __init__(self, env, log=True, goal_bonus=0):
         super().__init__(env)
         self.log = log
+        self.goal_bonus = goal_bonus
 
     def step(self, action):
         obs, _, terminated, truncated, info = self.env.step(action)
@@ -13,4 +14,8 @@ class RewardByScore(gym.Wrapper):
             reward = np.sum(np.log2(np.array(info['score_per_step']))) if len(info['score_per_step']) != 0 else 0
         else:
             reward = np.sum(np.array(info['score_per_step'])) if len(info['score_per_step']) != 0 else 0
+
+        if info['max'] == self.board_goal:
+            reward += self.goal_bonus
+
         return obs, reward, terminated, truncated, info
