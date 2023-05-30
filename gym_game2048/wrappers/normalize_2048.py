653a944f668ea1a5793c29ab3e0145c32e4645cf
helpingstar <54899900+helpingstar@users.noreply.github.com>
2023-05-30 17:07:16 +0900

    fix identation

--- gym_game2048/wrappers/normalize_2048.py
+++ gym_game2048/wrappers/normalize_2048.py
@@ -2,12 +2,15 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 
+
 # Normalize the observation.
 class Normalize2048(gym.ObservationWrapper):
     def __init__(self, env):
 
         super().__init__(env)
-        self.observation_space = spaces.Box(shape=self.observation_space.shape, low=0, high=1, dtype=np.float32)
+        self.observation_space = spaces.Box(
+            shape=self.observation_space.shape, low=0, high=1, dtype=np.float32
+            )
 
     def observation(self, obs):
         return obs / self.board_goal
