15415780bfc88ed69380f9feeed09f9d5bc56291
helpingstar <iamhelpingstar@gmail.com>
2024-03-07 10:29:28 +0900

    Update wrappers for support gymnasium >= 1.0.0

--- gym_game2048/wrappers/normalize_2048.py
+++ gym_game2048/wrappers/normalize_2048.py
@@ -8,9 +8,8 @@ class Normalize2048(gym.ObservationWrapper):
     def __init__(self, env):
 
         super().__init__(env)
-        self.observation_space = spaces.Box(
-            shape=self.observation_space.shape, low=0, high=1, dtype=np.float32
-            )
+        self.board_goal = env.get_wrapper_attr("board_goal")
+        self.observation_space = spaces.Box(shape=self.observation_space.shape, low=0, high=1, dtype=np.float32)
 
     def observation(self, obs):
         return obs / self.board_goal
