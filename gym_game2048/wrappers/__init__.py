d4c68019c698d51ca0355a1d706c1c0f743a80e7
helpingstar <iamhelpingstar@gmail.com>
2024-03-07 20:05:25 +0900

    Update reward wrappers

--- gym_game2048/wrappers/__init__.py
+++ gym_game2048/wrappers/__init__.py
@@ -1,4 +1,3 @@
 from gym_game2048.wrappers.normalize_2048 import Normalize2048
 from gym_game2048.wrappers.reward_converter import RewardConverter
-from gym_game2048.wrappers.reward_by_score import RewardByScore
-from gym_game2048.wrappers.terminate_illegal import TerminateIllegalWrapper
+from gym_game2048.wrappers.terminate_illegal import TerminateIllegal
--- gym_game2048/wrappers/reward_converter.py
+++ gym_game2048/wrappers/reward_converter.py
@@ -2,17 +2,22 @@ import gymnasium as gym
 import numpy as np
 
 
-class RewardConverter(gym.RewardWrapper):
-    def __init__(self, env, goal=5.0, fail=-5, other=-0.001):
+class RewardConverter(gym.Wrapper):
+    def __init__(self, env, div_pos_rew=2048, default_neg=0, term_rew=-1):
         super().__init__(env)
-        self.goal = goal
-        self.fail = fail
-        self.other = other
+        assert div_pos_rew > 0, "div_pos_rew must be greater than 0"
+        self.div_pos_rew = div_pos_rew
+        self.default_neg = default_neg
+        self.term_rew = term_rew
 
-    def reward(self, reward):
-        if reward == 0:
-            return self.other
-        elif reward > 0:
-            return self.goal
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        if not (terminated or truncated):
+            if len(info["score_per_step"]) == 0:
+                reward = self.default_neg
+            else:
+                reward = np.sum(np.array(info["score_per_step"])) / self.div_pos_rew
         else:
-            return self.fail
+            reward = self.term_rew
+
+        return obs, reward, terminated, truncated, info
