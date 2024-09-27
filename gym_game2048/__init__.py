0568b4e9d1575ae62dacb29922de1d56ebe93a38
helpingstar <iamhelpingstar@gmail.com>
2024-09-28 08:42:14 +0900

    Ruff

--- gym_game2048/__init__.py
+++ gym_game2048/__init__.py
@@ -3,6 +3,6 @@ from gymnasium.envs.registration import register
 __version__ = "0.0.1"
 
 register(
-     id="gym_game2048/Game2048-v0",
-     entry_point="gym_game2048.envs:Game2048",
+    id="gym_game2048/Game2048-v0",
+    entry_point="gym_game2048.envs:Game2048",
 )
--- gym_game2048/wrappers/normalize_2048.py
+++ gym_game2048/wrappers/normalize_2048.py
@@ -6,7 +6,6 @@ import numpy as np
 # Normalize the observation.
 class Normalize2048(gym.ObservationWrapper):
     def __init__(self, env):
-
         super().__init__(env)
         self.board_goal = env.get_wrapper_attr("board_goal")
         self.observation_space = spaces.Box(shape=self.observation_space.shape, low=0, high=1, dtype=np.float32)
