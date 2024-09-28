015c05ace6e4d0d9640a5ba81d947e57ff28c060
helpingstar <iamhelpingstar@gmail.com>
2024-09-28 09:45:50 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name="gym-game2048",
-    version="0.1.0",
-    author="helpingstar",
-    author_email="iamhelpingstar@gmail.com",
-    description="A reinforcement learning environment for the 2048 game based on Gymnasium.",
-    license="MIT License",
-    install_requires=["numpy>=1.21.0", "gymnasium>=1.0.0a1", "moviepy>=1.0.0", "pygame>=2.1.3"],
-    python_requires=">=3.9",
-)
+if __name__ == "__main__":
+    setuptools.setup()
